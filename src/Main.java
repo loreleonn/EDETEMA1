7c8d75ad1817af891db3ccb33e5f8c7034c6b204
loreleonn <lleoncam@myuax.com>
2025-03-07 11:29:18 +0100

    viernes 7 de marzo

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main { //nunca se le pide nada al usuario fuera del main
     //ejemplo metods estaticos
     public static int ejSuma(int a, int b){
         return a+b;
     }
 
-    public static int CalculoSegundos(int years, int meses, int dias){
+    public static int CalcSegundos(int years, int meses, int dias){
 
-        int diasTotales = (years * 365) + (meses * 30) + dias;
-        int segundos = diasTotales * 24 * 60 * 60;
-        return segundos;
+        return ((years * 365) + (meses * 30) + dias) * 24 * 60 * 60;
     }
-
 
     public static void main(String[] args) {
         //ejercicio 1 opcion 1, sin usar metodos
@@ -26,21 +23,22 @@ public class Main {
         System.out.println(a)*/
 
         int segs_years1 = dias1 * horas1 * mins1 * segs1;
-        System.out.println(segs_years1);
+        System.out.println("segundos en un año: "+segs_years1);
 
         /*los metodos estaticos o metodos de clase, pueden
         ser llamados sin que haya objetos de esta clase*/
 
+        //ejemplo metodo estatico
         int num = 0;
         num = ejSuma(10,2);
         System.out.println(num);
 
-        // OPCION 2 sin inputs
+        // opcion 2 ejercicio 1 sin inputs
         int segundosFecha = 0;
-        segundosFecha = CalculoSegundos(200, 2, 3);
-        System.out.println(segundosFecha);
+        segundosFecha = CalcSegundos(2, 2, 2);
+        System.out.println("total de segundos1: "+ segundosFecha);
 
-        //opcion 2 con inputs
+        //opcion 2 ejercicio 1 con inputs
         Scanner scanner = new Scanner(System.in);
         System.out.print("introducir años: ");
         int years = scanner.nextInt();
@@ -49,8 +47,17 @@ public class Main {
         System.out.print(" introducir dias: ");
         int dias = scanner.nextInt();
         scanner.close();
+        System.out.println("total de segundos2: " + CalcSegundos(years, meses, dias));
 
-        System.out.println("Total de segundos: " + CalculoSegundos(years, meses, dias));
+
+        //ejercicio 2
+        System.out.println("maximo valor de un char: " + (int) Character.MAX_VALUE);
+        System.out.println("maximo valor de un short: " + Short.MAX_VALUE);
+        System.out.println("maximo valor de un int: " + Integer.MAX_VALUE);
+        System.out.println("maximi valor de un byte: " + Byte.MAX_VALUE); // tambien puse el byte ya de una vez
+
+
+
 
 
     }
